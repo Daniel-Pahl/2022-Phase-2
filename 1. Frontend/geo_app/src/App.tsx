3e1149dd03e1a8ba121326b5bf2d67ea8ede1fec
Daniel-Pahl <65693019+Daniel-Pahl@users.noreply.github.com>
2022-08-08 15:01:14 +1200

    Closer to finishing the UI

--- 1. Frontend/geo_app/src/App.tsx
+++ 1. Frontend/geo_app/src/App.tsx
@@ -15,21 +15,33 @@ function App() {
   const axios = require("axios");
   return (
     <div className="App">
-      <h1>
-        City facts
-      </h1>
+      <h1>Country Information</h1>
       <div>
-        <label>Country Code</label><br/>
-        <input type="text" id="city-name" name="city-name" onChange={e => setCityName(e.target.value)}/><br/>
-        <button onClick={search}>
-        Search
-        </button>
+        <TextField
+          id="search-bar"
+          className="text"
+          value={cityName}
+          onChange={(prop: any)=> {setCityName(prop.
+            target.value)
+          }}
+          label="Enter a country code."
+          variant="outlined"
+          placeholder="Search"
+          size = "small"
+          />
+        <IconButton
+        aria-label='Search'
+        onClick={() =>{
+          search();
+        }}> 
+          <SearchIcon style={{fill: "brown"}} />
+        </IconButton>
       </div>
 
       <p>
-        You have entered {cityName}
+        You have entered {cityName.toUpperCase()}
       </p>
-      <div id="city-name">This will show the result</div>
+
 
     
     {countryInfo === undefined ?
@@ -39,14 +51,25 @@ function App() {
     </p>
 ) :(
     <div id="Country-result">
+      <img id = "flagLeft" src= {countryInfo.flag.file} 
+      />
       <p>
-        {countryInfo.capital.name}
-        <img src= {countryInfo.flag.file}  />
+        Capital City: {countryInfo.capital.name}
+      </p>  
+      <p>
+        Population: {countryInfo.population}
       </p>
+      <p>
+        Currency: {countryInfo.currency.name}
+      </p>
+      <a href= {countryInfo.wiki_url} > {countryInfo.name} wiki
+      </a>
+      {/* <img id = "flagRight" src= {countryInfo.flag.file} 
+      /> */}
             </div>
   )}
             </div>
-);
+  )
   
   function search(){
     const options = {
@@ -70,6 +93,10 @@ function App() {
   //     console.log(res.data);
   //     setCountryInfo(res.data)
   }
-  }
+}
 
 export default App;
+function round(arg0: number, arg1: number): React.ReactNode {
+  throw new Error('Function not implemented.');
+}
+
